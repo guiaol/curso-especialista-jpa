dfcafdeeb1b91ab2e9869a2a9c4adedfba42aae0
guiaol <arantesbhz@gmail.com>
2021-02-06 16:28:11 -0300

    Explicando a estrategia @GenerationVAlue (strategy=GenerationType.SEQUENCE, generator...

--- 03.08-configurando-a-geracao-de-identificador-com-sequencegenerator/src/test/java/com/algaworks/ecommerce/mapeamentobasico/EstrategiaChavePrimariaTest.java
+++ 03.08-configurando-a-geracao-de-identificador-com-sequencegenerator/src/test/java/com/algaworks/ecommerce/mapeamentobasico/EstrategiaChavePrimariaTest.java
@@ -2,14 +2,13 @@ package com.algaworks.ecommerce.mapeamentobasico;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Categoria;
-import com.algaworks.ecommerce.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class EstrategiaChavePrimariaTest extends EntityManagerTest {
 
     @Test
-    public void testarEstrategiaSequence() {
+    public void testarEstrategiaChave() {
         Categoria categoria = new Categoria();
         categoria.setNome("Eletrônicos");
 
@@ -22,19 +21,4 @@ public class EstrategiaChavePrimariaTest extends EntityManagerTest {
         Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
         Assert.assertNotNull(categoriaVerificacao);
     }
-
-//    @Test
-//    public void testarEstrategiaAuto() {
-//        Categoria categoria = new Categoria();
-//        categoria.setNome("Eletrônicos");
-//
-//        entityManager.getTransaction().begin();
-//        entityManager.persist(categoria);
-//        entityManager.getTransaction().commit();
-//
-//        entityManager.clear();
-//
-//        Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
-//        Assert.assertNotNull(categoriaVerificacao);
-//    }
 }
--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -14,10 +14,15 @@ import javax.persistence.*;
 public class Categoria {
     @EqualsAndHashCode.Include
     @Id
-    // estrategia para geracao de valores para chaves primarias.
-    // usado apenas para atributos marcados como chaves primarias.
-    // nesta estrategia a implementacao da especificacao JPA, no caso hibernate, que escolhera a estrategia para geracao dos id´s.
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    // nesta estrategia usa a geracao de uma sequence. O myslq nao tem sequence, mas o hibernate consegue simular uma.
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
+    // generator - nome do gerador da sequence
+    // name - bate com o nomedo gerador da sequence
+    // sequenceName - nome da tabela gerada pelo implementador do JPA ou o nome da sequence  existente no banco de dados
+    // initialValue - valor inicial da sequence
+    // allocationSize = 50 - aloca na memoria ate os proximos 50 ids, chegando nesse valor, vai ao banco buscar os proximos 50.
+    //                  quando esta comecando, ele vai na tabela e busca o 0, so quando chega no 50 que novamente na tabela e assim por diante.
+    @SequenceGenerator(name = "seq", sequenceName = "sequencia_chave_primaria") //, allocationSize = 50, initialValue = 500)
     private Integer id;
 
     private String nome;
