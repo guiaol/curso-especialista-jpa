657db39e08c8370cf687cd5d0e1a715e35a35959
guiaol <arantesbhz@gmail.com>
2021-02-18 21:33:09 -0300

    Exercicios Opcional

--- auxiliar-curso-especialista-jpa/04.14-exercicio-optional/src/test/java/com/algaworks/ecommerce/mapeamentobasico/MapeamentoObjetoEmbutidoTest.java
+++ auxiliar-curso-especialista-jpa/04.14-exercicio-optional/src/test/java/com/algaworks/ecommerce/mapeamentobasico/MapeamentoObjetoEmbutidoTest.java
@@ -15,18 +15,18 @@ public class MapeamentoObjetoEmbutidoTest extends EntityManagerTest {
 
     @Test
     public void analisarMapeamentoObjetoEmbutido() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+
         EnderecoEntregaPedido endereco = new EnderecoEntregaPedido();
-        endereco.setCep("00000-000");
+        endereco.setCep("00000-00");
         endereco.setLogradouro("Rua das Laranjeiras");
         endereco.setNumero("123");
         endereco.setBairro("Centro");
         endereco.setCidade("Uberlândia");
         endereco.setEstado("MG");
 
-        Cliente cliente = entityManager.find(Cliente.class, 1);
-
         Pedido pedido = new Pedido();
-//        pedido.setId(1); // comentando porque estamos utilizando IDENTITY
+        // pedido.setId(1); Comentado porque estamos utilizando IDENTITY
         pedido.setDataPedido(LocalDateTime.now());
         pedido.setStatus(StatusPedido.AGUARDANDO);
         pedido.setTotal(new BigDecimal(1000));
