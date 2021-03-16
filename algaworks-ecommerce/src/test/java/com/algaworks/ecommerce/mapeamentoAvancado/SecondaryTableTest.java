1e2cbac4a2c7a67e8ab263dcd55e651a9333b75f
guiaol <arantesbhz@gmail.com>
2021-03-15 21:22:28 -0300

    Mapeando tabela secundária com @SecondaryTable

---
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/mapeamentoAvancado/SecondaryTableTest.java
@@ -0,0 +1,27 @@
+package com.algaworks.ecommerce.mapeamentoAvancado;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.SexoCliente;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class SecondaryTableTest extends EntityManagerTest {
+
+    @Test
+    public void salvarCliente() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Carlos Finotti");
+        cliente.setSexo(SexoCliente.MASCULINO);
+        cliente.setDataNascimento(LocalDate.of(1990, 1, 1));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(cliente);
+        entityManager.getTransaction().commit();
+
+        Cliente clienteVeriicacao = entityManager.find(Cliente.class, cliente.getId());
+        Assert.assertNotNull(clienteVeriicacao.getSexo());
+    }
+}
