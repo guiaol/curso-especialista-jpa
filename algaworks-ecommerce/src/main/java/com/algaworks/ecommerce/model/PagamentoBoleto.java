f361914e69f2c94659a53e32fc218b33e1393a5f
guiaol <arantesbhz@gmail.com>
2021-03-20 11:49:54 -0300

    Mapeando herança com estratégia de tabela única (single table)

--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
@@ -4,13 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Getter
 @Setter
+@DiscriminatorValue("boleto")
 @Entity
-@Table(name = "pagamento_boleto")
+//@Table(name = "pagamento_boleto")
 public class PagamentoBoleto  extends Pagamento {
 
     @Column(name = "codigo_barras")
--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
@@ -4,13 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Getter
 @Setter
+@DiscriminatorValue("cartao")
 @Entity
-@Table(name = "pagamento_cartao")
+//@Table(name = "pagamento_cartao")
 public class PagamentoCartao extends Pagamento {
 
     @Column(name = "numero_cartao")
