2875a926335446287dc2ac25216777f6bef1414f
guiaol <arantesbhz@gmail.com>
2021-04-20 06:49:13 -0300

    Configurando detalhes da tabela com @Table

--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Cliente.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Cliente.java
@@ -12,10 +12,13 @@ import java.util.Map;
 @Setter
 @SecondaryTable(name = "cliente_detalhe", pkJoinColumns = @PrimaryKeyJoinColumn(name = "cliente_id"))
 @Entity
-@Table(name = "cliente")
+@Table(name = "cliente", uniqueConstraints = { @UniqueConstraint(name = "unq_cpf", columnNames = {"cpf"})},
+        indexes = { @Index(name = "idx_nome", columnList = "nome") })
 public class Cliente extends EntidadeBaseInteger {
 
     private String nome;
+
+    private String cpf;
 
     @ElementCollection
     @CollectionTable(name = "cliente_contato", joinColumns = @JoinColumn(name = "cliente_id"))
