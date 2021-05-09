53749faf54149837446b6decb67e94489bbec5e1
guiaol <arantesbhz@gmail.com>
2021-05-09 16:32:30 -0300

    Selecionando um atributo da entidade como retorno da consulta

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
@@ -1,7 +1,9 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,6 +12,20 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class BasicoJPQLTest extends EntityManagerTest {
+
+    @Test
+    public void selecionarUmAtributoParaRetorno() {
+        String jpql = "select p.nome from Produto p";
+
+        TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
+        List<String> lista = typedQuery.getResultList();
+        Assert.assertTrue(String.class.equals(lista.get(0).getClass()));
+
+        String jpqlCliente = "select p.cliente from Pedido p";
+        TypedQuery<Cliente> typedQueryCliente = entityManager.createQuery(jpqlCliente, Cliente.class);
+        List<Cliente> listaClientes = typedQueryCliente.getResultList();
+        Assert.assertTrue(Cliente.class.equals(listaClientes.get(0).getClass()));
+    }
 
     @Test
     public void buscarPorIdentificador() {
@@ -39,6 +55,5 @@ public class BasicoJPQLTest extends EntityManagerTest {
 
         List<Pedido> lista = query.getResultList();
         Assert.assertFalse(lista.isEmpty());
-
     }
 }
