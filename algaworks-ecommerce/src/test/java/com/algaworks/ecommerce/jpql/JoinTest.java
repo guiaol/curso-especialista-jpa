e8e01ecd1027280ed059a3b45e123e17c50990bf
guiaol <arantesbhz@gmail.com>
2021-05-11 22:14:44 -0300

    Fazendo inner join entre as entidades

---
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
@@ -0,0 +1,43 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.dto.ProdutoDTO;
+import com.algaworks.ecommerce.model.Pedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class JoinTest extends EntityManagerTest {
+
+    @Test
+    public void fazerJoinComProjecao_() {
+        String jpql = "select p, i from Pedido p join p.itens i";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assert.assertTrue(lista.size() == 3);
+    }
+
+    @Test
+    public void fazerJoinComProjecao() {
+        String jpql = "select p, pag from Pedido p join p.pagamento pag where pag.status = 'PROCESSANDO'";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assert.assertTrue(lista.size() == 1);
+    }
+
+    @Test
+    public void fazerJoin() {
+        String jpql = "select p from Pedido p join p.pagamento pag ";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        List<Pedido> lista = typedQuery.getResultList();
+
+        Assert.assertTrue(lista.size() == 1);
+    }
+}
