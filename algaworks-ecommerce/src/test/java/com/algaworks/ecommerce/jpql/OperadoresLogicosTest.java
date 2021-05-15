380fed194bd534eff8ad978019630402da802e3e
guiaol <arantesbhz@gmail.com>
2021-05-15 13:46:43 -0300

    Usando operadores lógicos

---
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/OperadoresLogicosTest.java
@@ -0,0 +1,28 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class OperadoresLogicosTest extends EntityManagerTest {
+
+    @Test
+    public void usarOperadores() {
+//        String jpql = "select p from Pedido p where p.total > 500 and p.status = 'AGUARDANDO' and p.cliente.id = 1";
+
+//        String jpql = "select p from Pedido p where p.status = 'PAGO' or p.status = 'AGUARDANDO'";
+
+        String jpql = "select p from Pedido p where p.status = 'PAGO' or p.status = 'AGUARDANDO' and p.total > 100";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        List<Pedido> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+}
