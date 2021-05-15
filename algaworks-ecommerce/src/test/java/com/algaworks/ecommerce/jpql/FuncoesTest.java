4d362e0b7b19629c6901300e73ff2a568e686815
guiaol <arantesbhz@gmail.com>
2021-05-15 15:14:47 -0300

    Usando funções para números

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
@@ -12,6 +12,21 @@ import java.util.TimeZone;
 public class FuncoesTest extends EntityManagerTest {
 
     @Test
+    public void aplicarFuncaoNumero() {
+        // abs, mod, sqrt
+
+//        String jpql = "select abs(-10), mod(3, 2), sqrt(9) from Pedido p";
+        String jpql = "select abs(p.total), mod(p.id, 2), sqrt(p.total) from Pedido p";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+    }
+
+    @Test
     public void aplicarFuncaoData() {
 
 //        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
