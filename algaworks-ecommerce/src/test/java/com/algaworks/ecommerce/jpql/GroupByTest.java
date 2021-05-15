5e645ff589bb7613301022526327ac33f1f63901
guiaol <arantesbhz@gmail.com>
2021-05-15 16:25:08 -0300

    Exercício: usando group by

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -18,9 +18,25 @@ public class GroupByTest extends EntityManagerTest {
 //        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) from Pedido p group by year(p.dataCriacao), month(p.dataCriacao)";
 
         // total de vendas por categoria
-        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip join ip.produto pro join pro.categorias c group by c.id";
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip join ip.produto pro join pro.categorias c group by c.id";
+
+        // total de vendas por cliente
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip join ip.pedido ped join ped.cliente c group by c.id";
+//        Fernando Medeiros, 5898.00
+//        Marcos Mariano, 499.00
+
+        // total de vendas por dia e por categoria
+//        String jpql = "select day(pro.dataCriacao), sum(ip.precoProduto) from ItemPedido ip join ip.produto pro join pro.categorias c group by c.id";
+        String jpql = "select " +
+                " concat(year(p.dataCriacao), '/', month(p.dataCriacao), '/', day(p.dataCriacao)), " +
+                " concat(c.nome, ': ', sum(ip.precoProduto)) " +
+                " from ItemPedido ip join ip.pedido p join ip.produto pro join pro.categorias c " +
+                " group by year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao), c.id " +
+                " order by p.dataCriacao, c.nome ";
 
 
+//        14, 1497.00
+//        14, 4900.00
 
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
