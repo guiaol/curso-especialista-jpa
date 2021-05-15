6abb6927442571a98b33170d509970bc3889a035
guiaol <arantesbhz@gmail.com>
2021-05-15 15:08:13 -0300

    Usando funções para datas

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
@@ -7,11 +7,29 @@ import org.junit.Test;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.TimeZone;
 
-public class FuncoesStringTest extends EntityManagerTest {
+public class FuncoesTest extends EntityManagerTest {
 
     @Test
-    public void aplicarFuncao() {
+    public void aplicarFuncaoData() {
+
+//        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+//        current_date, current_time, current_timestamp
+
+        String jpql = "select year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao) from Pedido p";
+//        String jpql = "select hour(p.dataCriacao), minute(p.dataCriacao), second(p.dataCriacao) from Pedido p";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+    }
+
+    @Test
+    public void aplicarFuncaoString() {
         // concat, length, locate, substring, lower, upper, trim
 
 //        String jpql = "select c.nome, concat('Categoria: ', c.nome) from Categoria c ";
