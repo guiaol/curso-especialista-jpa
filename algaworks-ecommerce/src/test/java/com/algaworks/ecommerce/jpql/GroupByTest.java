229706231a37029a08c9306a95ab327b2f2be053
guiaol <arantesbhz@gmail.com>
2021-05-15 16:10:20 -0300

    Agrupando o registros com group by

---
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -0,0 +1,33 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class GroupByTest extends EntityManagerTest {
+
+    @Test
+    public void agruparResultado() {
+        // quantidade de produtos por categoria
+//        String jpql = "select c.nome, count(c.id) from Categoria c join c.produtos p group by c.id";
+
+        // total de vendas no mes
+//        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) from Pedido p group by year(p.dataCriacao), month(p.dataCriacao)";
+
+        // total de vendas por categoria
+        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip join ip.produto pro join pro.categorias c group by c.id";
+
+
+
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+}
