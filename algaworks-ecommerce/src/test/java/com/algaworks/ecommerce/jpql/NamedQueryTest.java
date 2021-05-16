63d9086bd3e62c576c582dacc4bb999dd55bb188
guiaol <arantesbhz@gmail.com>
2021-05-16 13:04:27 -0300

    Externalizando queries em um arquivo XML

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/NamedQueryTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/NamedQueryTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,6 +10,30 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class NamedQueryTest extends EntityManagerTest {
+
+    @Test
+    public void executarConsultaArquivoXmlEspecificoProduto() {
+        TypedQuery<Produto> typedQuery = entityManager.createNamedQuery("Produto.todos", Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void executarConsultaArquivoXmlEspecificoPedido() {
+        TypedQuery<Pedido> typedQuery = entityManager.createNamedQuery("Pedido.todos", Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void executarConsultaArquivoXml() {
+        TypedQuery<Pedido> typedQuery = entityManager.createNamedQuery("Pedido.listar", Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
 
     @Test
     public void executarConsulta() {
