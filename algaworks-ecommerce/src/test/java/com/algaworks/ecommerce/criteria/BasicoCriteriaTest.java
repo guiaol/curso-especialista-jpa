3b3aa739f682510323bedebd5f37e44cf6a8e514
guiaol <arantesbhz@gmail.com>
2021-05-19 20:40:48 -0300

    Selecionando um atributo da entidade como retorno da consulta

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
@@ -1,7 +1,9 @@
 package com.algaworks.ecommerce.criteria;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
+import org.hibernate.Criteria;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,12 +11,43 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.math.BigDecimal;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
 
     @Test
-    public void buscarPorIdentificador() {
+    public void selecionarUmAtributoParaRetorno() {
 
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<BigDecimal> criteriaQuery = criteriaBuilder.createQuery(BigDecimal.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root.get("total")); // select p.total from Pedido p where p.id = 1;
+
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<BigDecimal> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        BigDecimal total = typedQuery.getSingleResult();
+        Assert.assertEquals(new BigDecimal("2398.00"), total);
+
+
+//        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+//        CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+//        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+//
+//        criteriaQuery.select(root.get("cliente")); // select p.cliente from Pedido p where p.id = 1;
+//
+//        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+//
+//        TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
+//
+//        Cliente cliente = typedQuery.getSingleResult();
+//        Assert.assertEquals("Fernando Medeiros", cliente.getNome());
+    }
+
+    @Test
+    public void buscarPorIdentificador() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
         Root<Pedido> root = criteriaQuery.from(Pedido.class);
