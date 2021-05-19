668e8b43afd3c0a142a6b9acd180d66844e023c1
guiaol <arantesbhz@gmail.com>
2021-05-19 20:49:54 -0300

    Exercício: retornando todos os produtos

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
@@ -3,6 +3,7 @@ package com.algaworks.ecommerce.criteria;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
 import org.hibernate.Criteria;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,8 +13,22 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void retornarTodosOsProdutosExercicio() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class); // select p from Produto p;
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
 
     @Test
     public void selecionarUmAtributoParaRetorno() {
