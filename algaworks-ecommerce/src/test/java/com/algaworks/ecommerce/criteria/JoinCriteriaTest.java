f04bffe9fd1571293f5cf1f2fa213e9a49e5fe01
guiaol <arantesbhz@gmail.com>
2021-05-20 21:21:27 -0300

    Fazendo inner join entre as entidades

---
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/criteria/JoinCriteriaTest.java
@@ -0,0 +1,87 @@
+package com.algaworks.ecommerce.criteria;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class JoinCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void fazerJoin() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento");
+        Join<Pedido, ItemPedido> joinItens = root.join("itens");
+        Join<ItemPedido, Produto> joinItemProduto = joinItens.join("produto");
+
+        // select p from Pedido p join p.pagamento pag
+//        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento");
+//        Join<Pedido, Cliente> joinCliente = root.join("cliente");
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertTrue(lista.size() == 4);
+
+
+
+
+//        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+//        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+//
+//        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+//
+//        // select p from Pedido p join p.pagamento pag
+//        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento");
+//
+//        criteriaQuery.select(root);
+//
+//        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+//        List<Pedido> lista = typedQuery.getResultList();
+//        Assert.assertTrue(lista.size() == 4);
+
+
+//        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+//        CriteriaQuery<Pagamento> criteriaQuery = criteriaBuilder.createQuery(Pagamento.class);
+//
+//        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+//
+//        // select p from Pedido p join p.pagamento pag
+//        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento");
+//
+//        criteriaQuery.select(joinPagamento);
+//        criteriaQuery.where(criteriaBuilder.equal(joinPagamento.get("status"), StatusPagamento.PROCESSANDO));
+//
+//        TypedQuery<Pagamento> typedQuery = entityManager.createQuery(criteriaQuery);
+//        List<Pagamento> lista = typedQuery.getResultList();
+//        Assert.assertTrue(lista.size() == 4);
+
+
+
+
+//        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+//        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+//
+//        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+//
+//        // select p from Pedido p join p.pagamento pag
+//        Join<Pedido, Pagamento> join = root.join("pagamento");
+//
+//        criteriaQuery.select(root);
+//
+//        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+//        List<Pedido> lista = typedQuery.getResultList();
+//        Assert.assertTrue(lista.size() == 4);
+    }
+}
