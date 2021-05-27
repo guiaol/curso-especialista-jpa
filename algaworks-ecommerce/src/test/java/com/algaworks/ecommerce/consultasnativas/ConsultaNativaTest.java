242b680d96141b00ee407ff831f355cc3ca40e9b
guiaol <arantesbhz@gmail.com>
2021-05-26 21:17:33 -0300

    Passando parâmetros para consulta nativa

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/consultasnativas/ConsultaNativaTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/consultasnativas/ConsultaNativaTest.java
@@ -10,6 +10,23 @@ import java.util.List;
 public class ConsultaNativaTest extends EntityManagerTest {
 
     @Test
+    public void passarParmetros() {
+        String sql = "select prd_id id, prd_nome nome, prd_descricao descricao, " +
+                "            prd_data_criacao data_criacao, prd_data_ultima_atualizacao data_ultima_atualizacao, " +
+                "            prd_preco preco, prd_foto foto " +
+                " from ecm_produto where prd_id = :id";
+
+
+        Query query = entityManager.createNativeQuery(sql, Produto.class);
+        query.setParameter("id", 201);
+
+        List<Produto> lista = query.getResultList();
+
+        lista.stream().forEach(obj -> System.out.println(
+                String.format("Produto => ID: %s, Nome: %s", obj.getId(), obj.getNome())));
+    }
+
+    @Test
     public void executarSQLRetornandoEntidade() {
 //        String sql = "select id, nome, descricao, data_criacao, data_ultima_atualizacao, preco, foto " +
 //                " from produto_loja";
