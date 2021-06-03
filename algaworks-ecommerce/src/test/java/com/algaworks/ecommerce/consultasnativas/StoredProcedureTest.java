9630598ed591745b4ed3bdd8c05abd8745cad849
guiaol <arantesbhz@gmail.com>
2021-06-03 13:59:46 -0300

    Configurando uma procedure com a anotação @NamedStoredProcedureQuery

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/consultasnativas/StoredProcedureTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/consultasnativas/StoredProcedureTest.java
@@ -13,6 +13,18 @@ import java.util.List;
 public class StoredProcedureTest extends EntityManagerTest {
 
     @Test
+    public void chamarNamedStoredProcedure() {
+        StoredProcedureQuery storedProcedureQuery = entityManager
+                .createNamedStoredProcedureQuery("compraram_acima_media");
+
+        storedProcedureQuery.setParameter("ano", 2020);
+
+        List<Cliente> lista = storedProcedureQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
     public void atualizarPrecoProdutoExercicio() {
         StoredProcedureQuery storedProcedureQuery = entityManager
                 .createStoredProcedureQuery("ajustar_preco_produto", Cliente.class);
