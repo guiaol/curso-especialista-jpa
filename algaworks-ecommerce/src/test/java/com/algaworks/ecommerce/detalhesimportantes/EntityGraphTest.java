b4977b0ae947bceac9c8d87d010b321dfd1b3b28
guiaol <arantesbhz@gmail.com>
2021-06-05 16:22:28 -0300

    Utilizando metamodel com Entity Graph

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/detalhesimportantes/EntityGraphTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/detalhesimportantes/EntityGraphTest.java
@@ -1,15 +1,36 @@
 package com.algaworks.ecommerce.detalhesimportantes;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.Cliente_;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Pedido_;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.EntityGraph;
+import javax.persistence.Subgraph;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class EntityGraphTest extends EntityManagerTest {
+
+    @Test
+    public void buscarAtributosEssenciaisDePedido03() {
+        EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
+        entityGraph.addAttributeNodes(
+                Pedido_.dataCriacao, Pedido_.status, Pedido_.total);
+
+        Subgraph<Cliente> subgraphCliente = entityGraph
+                .addSubgraph(Pedido_.cliente);
+        subgraphCliente.addAttributeNodes(Cliente_.nome, Cliente_.cpf);
+
+        TypedQuery<Pedido> typedQuery = entityManager
+                .createQuery("select p from Pedido p", Pedido.class);
+        typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
 
     @Test
     public void buscarAtributosEssenciaisDePedido02() {
