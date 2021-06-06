df7864176d573ea70d3ae1807755eb204afba709
guiaol <arantesbhz@gmail.com>
2021-06-06 15:16:28 -0300

    Incluindo as entidades no cache

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/cache/CacheTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/cache/CacheTest.java
@@ -24,6 +24,21 @@ public class CacheTest {
     }
 
     @Test
+    public void adicionarPedidosNoCache() {
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+
+        System.out.println("Buscando a partir da instância 1:");
+        entityManager1.createQuery("select p from Pedido p", Pedido.class)
+                .getResultList();
+
+        System.out.println("Buscando a partir da instância 2:");
+//        entityManager2.find(Pedido.class, 1);
+        entityManager2.createQuery("select p from Pedido p", Pedido.class)
+                .getResultList();
+    }
+
+    @Test
     public void buscarDoCache() {
         EntityManager entityManager1 = entityManagerFactory.createEntityManager();
         EntityManager entityManager2 = entityManagerFactory.createEntityManager();
