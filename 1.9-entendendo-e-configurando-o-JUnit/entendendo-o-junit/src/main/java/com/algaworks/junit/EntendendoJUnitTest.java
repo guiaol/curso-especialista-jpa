0d082158fd741579734cd5dce15d92b582802cda
guiaol <arantesbhz@gmail.com>
2023-11-08 21:51:06 -0300

    1.9. Entendendo e configurando o JUnit

---
+++ 1.9-entendendo-e-configurando-o-JUnit/entendendo-o-junit/src/main/java/com/algaworks/junit/EntendendoJUnitTest.java
@@ -0,0 +1,40 @@
+package com.algaworks.junit;
+
+
+import org.junit.jupiter.api.*;
+
+public class EntendendoJUnitTest {
+
+    @BeforeAll
+    public static void iniciarTestes() {
+        System.out.println(">>>  public static void iniciarTestes() <<<");
+    }
+
+    @AfterAll
+    public static void encerrarTestes() {
+        System.out.println(">>>  public static void encerrarTestes() <<<");
+    }
+
+    @BeforeEach
+    public void iniciarTeste() {
+        System.out.println(">>>  public void iniciarTeste() <<<");
+    }
+
+    @AfterEach
+    public void encerrarTeste() {
+        System.out.println(">>>  public void encerrarTeste() <<<");
+    }
+
+    @Test
+    public void testandoAlgo() {
+        String nome = String.format("%s", "Guilherme");
+        Assertions.assertEquals("Guilherme", nome);
+    }
+
+    @Test
+    public void testandoOutraColisa() {
+        String str = String.format("%s", "");
+
+        Assertions.assertTrue(str.isEmpty());
+    }
+}
