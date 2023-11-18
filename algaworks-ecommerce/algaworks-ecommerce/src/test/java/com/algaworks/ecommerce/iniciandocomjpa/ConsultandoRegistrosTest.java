d98b81c04c9f49796dd5cf457dcb142df729f2c7
guiaol <arantesbhz@gmail.com>
2023-11-17 22:01:54 -0300

    2.6. Buscando objetos por identificador

--- algaworks-ecommerce/algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ algaworks-ecommerce/algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -1,12 +1,10 @@
 package com.algaworks.ecommerce.iniciandocomjpa;
 
+import com.algaworks.ecommerce.model.Produto;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 
 public class ConsultandoRegistrosTest {
 
@@ -32,6 +30,27 @@ public class ConsultandoRegistrosTest {
     @AfterEach
     public void tearDown() {
         em.close();;
+    }
+
+    @Test
+    public void buscarPorIdentificador() {
+        Produto produto = em.find(Produto.class, 1);
+//        Produto produto = em.getReference(Produto.class, 1);
+
+//         System.out.println("Ainda não buscou!!!");
+
+        Assertions.assertNotNull(produto);
+        Assertions.assertEquals("Kindle", produto.getNome());
+    }
+
+    @Test
+    public void atualizarAReferencia() {
+        Produto produto = em.find(Produto.class, 1);
+        produto.setNome("Microfone Samson");
+
+        em.refresh(produto);
+
+        Assertions.assertEquals("Kindle", produto.getNome());
     }
 
 }
